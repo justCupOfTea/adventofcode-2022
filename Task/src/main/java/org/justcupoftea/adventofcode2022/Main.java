fc57e51b6692af8b24248393415f0f2b407fda5f
Pol Marvel <polmarvel91@gmail.com>
2022-12-03 22:47:02 +0300

    day 1

--- Task/src/main/java/org/justcupoftea/adventofcode2022/Main.java
+++ Task/src/main/java/org/justcupoftea/adventofcode2022/Main.java
@@ -1,16 +1,18 @@
 package org.justcupoftea.adventofcode2022;
 
 import lombok.extern.slf4j.Slf4j;
+import org.justcupoftea.adventofcode2022.day.Day1;
 import org.justcupoftea.adventofcode2022.task.AbstractTask;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 public class Main {
 
-    private static final List<AbstractTask<?>> TASK_LIST = List.of();
+    private static final List<AbstractTask<?>> TASK_LIST = List.of(
+            new Day1()
+    );
 
     public static void main(String[] args) {
         TASK_LIST.forEach(AbstractTask::printAnswers);
---
+++ Task/src/main/java/org/justcupoftea/adventofcode2022/day/Day1.java
@@ -0,0 +1,34 @@
+package org.justcupoftea.adventofcode2022.day;
+
+import org.justcupoftea.adventofcode2022.task.AbstractTask;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Day1 extends AbstractTask<Integer> {
+    @Override
+    protected List<Integer> mapData(String data) {
+        return Arrays.stream(data.split("\n\n"))
+                .map(s -> Arrays.stream(s.split("\n"))
+                        .map(Integer::parseInt)
+                        .reduce(0, Integer::sum))
+                .sorted(Comparator.reverseOrder())
+                .toList();
+    }
+
+    @Override
+    protected int getDayNumber() {
+        return 1;
+    }
+
+    @Override
+    protected String getPartOneAnswer(List<Integer> data) {
+        return String.valueOf(data.get(0));
+    }
+
+    @Override
+    protected String getPartTwoAnswer(List<Integer> data) {
+        return data.stream().limit(3).reduce(0, Integer::sum).toString();
+    }
+}
